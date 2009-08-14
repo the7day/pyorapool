a9fe0d7e398dd785b0d3860099335690763f5b4d
contact@tesobe.com <contact@tesobe.com@416a6e40-584d-0410-84c0-85a93026aac0>
2009-08-14 14:01:07 +0000

    make db backend work with django 1.1

--- trunk/oracle_pool.py
+++ trunk/oracle_pool.py
@@ -2,49 +2,38 @@
 
 # vim: ai ts=4 sts=4 et sw=4
 
-# Copyright (C) 2007-2008 Music Pictures Ltd 2007, 2008. 
-# Authors for this file:
-#                       Stefan Bethge <stefan@musicpictures.com>
-#                       Simon Redfern <simon@musicpictures.com>
+# This pooling server is there to keep db connections for django alive, because doesn't do
+# it and there is nothing like pgpool for oracle freely available.
+# The connections can't be held in the django application directly, because all the app
+# code is only run on a request basis. It would be necessary to change django's manage.py
+# but we don't want to do that.
+# Finally, the decision for pyro was made after a recommendation by Dietz.
 
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU Lesser General Public License as published
-## by the Free Software Foundation; version 3 or newer.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU Lesser General Public License for more details. 
+# Copyright: Music Pictures Ltd 2007, 2008. 
+# Author: Stefan Bethge, Simon Redfern
 
 import Pyro.core
+import signal
 import cx_Oracle
 import time
-
-try:
-    import uuid
-except:
-    #python < 2.5
-    from util.uuid import *
-
+from util.uuid import *
 from oracle_pool_procedures import *
 import logging as log
+
+import os
+os.environ['NLS_LANG'] = '.UTF8'
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+
+from django.conf import settings
+from django.utils.encoding import smart_str, force_unicode
 
 log.basicConfig(
     level=log.INFO,
     format='%(asctime)s %(levelname)-8s %(message)s',
     datefmt='%m-%d %H:%M',
-    filename='/var/log/pyorapool.log', 
+    filename='/var/log/calypso/oracle_pool_dev.log', 
     filemode='a'
 )
-
-import os
-os.environ['NLS_LANG'] = '.UTF8'
-
-#import django stuff
-os.environ['DJANGO_SETTINGS_MODULE']='settings'
-
-from django.conf import settings
-from django.utils.encoding import smart_str, force_unicode
 
 # timeout for unused connections in seconds
 timeout = settings.PYRO_POOL_TIMEOUT
@@ -52,7 +41,6 @@ timeout = settings.PYRO_POOL_TIMEOUT
 # timeout for connections that are locked but might be dead
 timeout_locked = settings.PYRO_POOL_LOCKED_TIMEOUT
 
-#the pool (tm)
 connections = []
 
 #serverside_cursors = []
@@ -66,25 +54,30 @@ def to_unicode(s):
         return force_unicode(s)
     return s
 
-class CursorWrapper(Pyro.core.ObjBase):
-    '''
-    wrapper classes for django backend (oracle_pyro)
-    '''
+#wrapper classes for django backend
+class CursorWrapper(Pyro.core.SynchronizedObjBase):
     cursor = None
     id = str(uuid1)
     charset = 'utf-8'
+    is_wrapper = True
+    daemon = None
+    #check for wrapper with hasattr(cursor, is_wrapper) ?
 
     def __init__(self, connection, *args, **kwargs):
-        Pyro.core.ObjBase.__init__(self)
+        Pyro.core.SynchronizedObjBase.__init__(self)
+        global daemon
+        self.daemon = daemon
+
         self.cursor = connection.cursor(*args, **kwargs)
         # This looks like a hard limit on number of cursors?
-
-        #TODO: create getter/setter method for this,
-        #so it can be set using the proxy
         self.cursor.arraysize = 100
 
-    def description(self, *args, **kwargs):
-        return self.cursor.description(*args, **kwargs)
+    def get_description(self):
+        result = self.cursor.description
+        log.debug('description: %s' % result)
+        return result
+
+    description = property(get_description)
 
     def _format_params(self, params):
         if isinstance(params, dict):
@@ -123,7 +116,7 @@ class CursorWrapper(Pyro.core.ObjBase):
         else:
             params = self._format_params(params)
 
-        log.debug('execute query %s with params %s' % (str(query), str(params)))
+        #log.debug('execute query %s with params %s' % (str(query), str(params)))
 
         ## taken from django source / oracle backend
         args = [(':arg%d' % i) for i in range(len(params))]
@@ -134,14 +127,12 @@ class CursorWrapper(Pyro.core.ObjBase):
         if query.endswith(';') or query.endswith('/'):
             query = query[:-1]
         query = smart_str(query, self.charset) % tuple(args)
-        #execute creates cx_Oracle.STRING/NUMBER/.. values, convert to python values
-        result = ''
         try:
-            result = self.cursor.execute(query, params)
-            #TODO: should result be returned, what for(no data in it)?
+            self.cursor.execute(query, params)
+            #result = self.cursor.fetchall()
+            log.debug('execute query %s with params %s' % (str(query), str(params)))
         except Exception, e:
             log.warning("Exception: %s" % e)
-        #return self.convert_cx_var(result)
 
     def executemany(self, query, params=None):
         try:
@@ -209,18 +200,22 @@ class CursorWrapper(Pyro.core.ObjBase):
                 serverside_cursors.remove(c)
     '''
 
-class ConnectionWrapper(Pyro.core.ObjBase):
-    '''
-    wrapper that can be passed as an object/proxy over pyro
-    '''
+class ConnectionWrapper(Pyro.core.SynchronizedObjBase):
     connection = None
     locked = False
     last_used = time.time()
     id = time.time()
+    daemon = None
 
     def __init__(self, *args, **kwargs):
-        Pyro.core.ObjBase.__init__(self)
-        self.connection = cx_Oracle.connect(threaded=True ,*args, **kwargs)
+        Pyro.core.SynchronizedObjBase.__init__(self)
+        global daemon
+        self.daemon = daemon
+        try:
+            self.connection = cx_Oracle.connect(threaded=True ,*args, **kwargs)
+        except cx_Oracle.DatabaseError, e:
+            #if ORA-12170 in e: #timeout
+            self.connection = None
     
     def release(self):
         log.debug('release %s' % id(self))
@@ -248,9 +243,9 @@ class ConnectionWrapper(Pyro.core.ObjBase):
         self.release()
         log.debug('close called')
 
-class StoredProcedures(Pyro.core.ObjBase):
+class StoredProcedures(Pyro.core.SynchronizedObjBase):
     def __init__(self):
-        Pyro.core.ObjBase.__init__(self)
+        Pyro.core.SynchronizedObjBase.__init__(self)
 
     def call(self, package, name, *args, **kwargs):
         klass = globals()[package]
@@ -260,22 +255,27 @@ class StoredProcedures(Pyro.core.ObjBase):
         try:
             return function(instance, *args, **kwargs)
         except Exception, e:
+            if 'not connected' in e or 'not logged on' in e:
+                log.debug('retry sp call because of error with connection...')
+                return function(instance, *args, **kwargs)
+
             import sys, traceback
             for line in traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback):
                 log.exception(line)
-            raise Exception(e)
+            raise e
 
     def status(self):
         global connections
+        #log.debug('list of connections follows')
+        #for c in connections:
+        #    log.debug('connection: %s, locked: %s, time: %s' % (id(c),c.locked, c.last_used))
         return '%d connection(s) in pool' % len(connections)
 
 
-class OraclePool(Pyro.core.ObjBase):
-    '''
-    provide remote connections, used in django backend
-    '''
+#used for providing remote connections, used in django backend
+class OraclePool(Pyro.core.SynchronizedObjBase):
     def __init__(self):
-        Pyro.core.ObjBase.__init__(self)
+        Pyro.core.SynchronizedObjBase.__init__(self)
 
     def connect(self):
         return internal_connect(return_proxy=True)
@@ -296,76 +296,93 @@ class OraclePool(Pyro.core.ObjBase):
     def status(self):
         return '%d connections in pool' % len(connections)
 
-def internal_connect(return_proxy=False):
+#really connect to oracle and pool the connection
+def internal_connect(return_proxy=False, force_connect=False):
     '''
-    connect to oracle and pool the connection
-    
-    set return_proxy to True to create and return a proxy object
-    for the connection
+        internal_connect
+            connect to oracle or give a free connection
+
+        accepts:
+            force_connect - connect even if there are free connections
+
+        returns:
+            connection wrapper object as a proxy or not
     '''
+
     global connections
     conn = None
     connections_count = 0
-    
+
     if connections:
         connections_count = len(connections)
-        
-    log.debug(('Looking for alive, non-locked oracle connection within %s connections ...')  % connections_count)
 
-    #sort connection list so we always us the oldest connections first
-    connections.sort(lambda x, y: cmp(x.last_used,y.last_used))
+    if not force_connect:
+        log.debug(('Starting to look for alive, non-locked oracle connection within %s connections ...')  % str(connections_count))
 
-    #look for free connection
-    for c in connections:
-        if c.locked == False:
-            conn = c
-            cursor = conn.internal_cursor()
-            try:
-                log.debug(('Testing existing connection %s with simple select.') % str(id(conn)))
-                log.debug('list of current connections:')
-                for c in connections:
-                    log.debug('connection: %s, locked: %s, time: %s' % (id(c),c.locked, c.last_used))
+        #sort connection list so we always us the oldest connections first
+        connections.sort(lambda x, y: cmp(x.last_used,y.last_used))
 
-                cursor.execute('select 1 from DUAL')
-                # Exception while testing existing connection: ORA-24909: call in progress
-                # However - ORA-03114: not connected to ORACLE is caught well.
-                ok = True
-            except Exception, e:
-                log.warning('Exception while testing existing connection: %s. I will set the conn to None so it will not be returned' % e)
-                ok = False
-                conn = None
-                c.locked = True #so it won't be used again and then times out
-            if ok:
-                log.debug(('tested existing connection %s and it is OK.') % str(id(conn)))
-                break
+        #look for free connection
+        for c in connections:
+            if c.locked == False:
+                conn = c
+                cursor = conn.internal_cursor()
+                try:
+                    log.debug(('Before testing an existing connection %s with simple select.') % str(id(conn)))
+                    log.debug('list of connections follows')
+                    for c in connections:
+                        log.debug('connection: %s, locked: %s, time: %s' % (id(c),c.locked, c.last_used))
+
+                    cursor.execute('select * from DUAL')
+                    # Exception while testing existing connection: ORA-24909: call in progress
+                    # However - ORA-03114: not connected to ORACLE is caught well.
+                    log.debug(('After testing an existing connection %s with simple select.') % str(id(conn)))
+                    ok = True
+                except Exception, e:
+                    # The problem here is that if an exception does occur it has probably "spoiled" the connection for whatever was using it.  
+                    log.warning('Exception while testing existing connection: %s. I will set the conn to None so it will not be returned' % e)
+                    ok = False
+                    conn = None
+                    c.locked = True #so it won't be used again and then times out
+                if ok:
+                    log.debug(('I tested an existing connection %s and it is OK.') % str(id(conn)))
+                    break
+
+    #return proxy if a connection that has been created before is wanted as a proxy now
+    if return_proxy and conn:
+        conn.getDaemon().connect(conn)
 
     #no free conn, create new connection
     if not conn:    
         conn = ConnectionWrapper(settings.DATABASE_USER_CX, settings.DATABASE_PASSWORD,'//%s/%s' % (settings.DATABASE_HOST, settings.DATABASE_NAME))
-        log.debug(('no free connection found, created a new one: %s ') % str(id(conn)))
+        log.debug(('Created a new connection: %s ') % str(id(conn)))
         if return_proxy:
-            self.getDaemon().connect(conn)
-        
-        conn.locked = True
+            conn.getDaemon().connect(conn)
+    
+        if not force_connect:
+            conn.locked = True
             
         connections.append(conn)
     else:
-        log.debug(('returning existing connection: %s ') % str(id(conn)))
+        log.debug(('I am returning the good existing connection: %s ') % str(id(conn)))
         
-    conn.locked = True
+    if not force_connect:
+        conn.locked = True
     conn.last_used = time.time()
     
     if return_proxy: 
+        log.debug(('Before return proxy'))
         return conn.getAttrProxy()
     else:
+        log.debug(('Before return'))
         return conn
 
+#connection handling callback
 def handle_connections(ins):
-    '''
-    callback that does the pooling and timeout logic
-    '''
     global connections
     #global daemon
+    
+    free_connections = 0
 
     #iterate over copy of connections
     for c in connections[:]:
@@ -381,25 +398,64 @@ def handle_connections(ins):
             connections.remove(c)
 
         elif time.time() - c.last_used > timeout:
-            if not c.locked:
+            # just trying explicit
+            if c.locked == False:
+            #if not c.locked:
                 log.debug('Disconnecting connection %s because of timeout' % str(id(c)))
                 try:
                     c.connection.close()
-                except DatabaseError, e:
+                except cx_Oracle.DatabaseError, e:
                     log.warning('Error while closing connection: %s' % e)
                 #daemon.disconnect(c)
                 log.debug('Removing connection object %s from pool' % str(id(c)))
                 connections.remove(c)
+
+        #see if there are free connections
+        if not c.locked:
+            free_connections+=1
+
+    if free_connections < settings.PYRO_POOL_MIN_SPARE_CONNECTIONS:
+        #add as many new connections
+        for i in range(0, settings.PYRO_POOL_MIN_SPARE_CONNECTIONS - free_connections):
+            internal_connect(force_connect=True)
+
+        log.debug('created new spare connection(s), list of connections follows')
+        for c in connections:
+            log.debug('connection: %s, locked: %s, time: %s' % (id(c),c.locked, c.last_used))
+
         
 Pyro.config.PYRO_PRINT_REMOTE_TRACEBACK = True
 Pyro.config.PYRO_DETAILED_TRACEBACK = True
+
+# Simon added 
+if settings.CURRENT_INSTALLATION_PROFILE in ('han_dev','han_live'):
+    Pyro.config.PYRO_HOST = 'localhost'
+    print 'Before Pyro.core.initServer'
+log.info('Before Pyro.core.initServer')
 
 Pyro.core.initServer()
 daemon=Pyro.core.Daemon()
 daemon.connect(OraclePool(),"OraclePool")
 uri=daemon.connect(StoredProcedures(), "StoredProcedures")
 
-log.info('pyorapool started on port:%s' % daemon.port)
+log.info('The daemon runs on port:%s' % daemon.port)
+log.info('The object\'s uri is:%s' % uri)
 
-daemon.requestLoop(callback=handle_connections)
+def onSignal(signum, stackframe):                 # handler for kill signals 
+    print 'Killed with signal %d. Shutting down.' % signum
+    #TODO: close open connections here
+    global connections
+    for c in connections:
+        c.close()
+    daemon.shutdown()
+
+signal.signal(1, onSignal) 
+signal.signal(2, onSignal) 
+signal.signal(3, onSignal) 
+signal.signal(15, onSignal) 
+
+try:
+    daemon.requestLoop(callback=handle_connections)
+except KeyboardInterrupt: 
+    daemon.shutdown()
 
