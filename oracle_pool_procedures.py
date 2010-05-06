ef317f03935fdd1841893584be002fc83f670958
contact <contact@tesobe.com>
2010-05-06 15:52:26 +0000

    implement proper mutlithreading a few bips and bops

--- oracle_pool_procedures.py
+++ oracle_pool_procedures.py
@@ -1,6 +1,7 @@
+# -*- coding: UTF-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 
-# Copyright (C) 2007-2008 Music Pictures Ltd 2007, 2008. 
+# Copyright (C) 2007-2010 Music Pictures Ltd
 # Authors for this file:
 #                       Stefan Bethge <stefan@musicpictures.com>
 #                       Simon Redfern <simon@musicpictures.com>
@@ -18,23 +19,15 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE']='settings'
 
 import logging as log
-
 import datetime
-
-#uuid is used for debug messages
-try:
-    import uuid
-except ImportError:
-    import util.uuid as uuid
+import util.uuid as uuid #for debugging
 
 """
 
 Note!! If you get an error like below:
- look into your error log and see the real traceback
+ look into the error.log and see the full traceback
 
 Traceback (most recent call last): 
-...
-
 File "/usr/lib/python2.4/site-packages/Pyro/core.py", line 444, in __invokePYRO__ constants.RIF_VarargsAndKeywords, vargs, kargs) 
 File "/usr/lib/python2.4/site-packages/Pyro/protocol.py", line 373, in remoteInvocation answer = pickle.loads(answer) UnpicklingError: NEWOBJ class argument has NULL tp_new
 
@@ -59,39 +52,29 @@ def create_dict_from_cursor_or_none(cursor):
             return_rec = None 
         return return_rec
 
-
 # template class for stored procedure/function wrappers
 # to be called via pyro from the client: StoredProcedures.call(package, function)
-
 # TODO: generalize this
 class package:
     def __init__(self, connect):
         self.cx_connect = connect
 
-    def fn_stored_function(self, parameter):
+    def fn_stored_function(self, cx_connection, parameter):
         cx_connection = self.cx_connect()
-
         cursor = cx_connection.internal_cursor()
         cx_parameter.setvalue(0, parameter.encode('UTF-8'))
         retVal = cursor.callfunc("package.stored_function", cx_Oracle.NUMBER, [cx_parameter])
         cursor.close()
-
-        #important: release connection so it can be used again
-        cx_connection.release()
-
+        #connection will be closed from the pool if necessary, so leave it open
         return retVal
-    
-    def pr_stored_procedure(self, parameter_in=None):
-        cx_connection = self.cx_connect()
 
+    def pr_stored_procedure(self, cx_connection, parameter_in=None):
+        cx_connection = self.cx_connect()
         cursor = cx_connection.internal_cursor()
         ref_cursor = cx_connection.internal_cursor()
         retVal = cursor.callproc("package.stored_procedure", [parameter_in, ref_cursor])
         data = ref_cursor.fetchall()
         cursor.close()
-
         ref_cursor.close()
-        cx_connection.release()
 
         return data
-    
